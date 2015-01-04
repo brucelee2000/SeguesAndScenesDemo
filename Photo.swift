5f14e4a2b7027f5fd85faf9d8d1926bd79a2f411
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-03 22:17:16 -0800

    Step2. Add files for data and each scene(VCs).

---
+++ Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  SeguesAndScenesDemo
+//
+//  Created by Yosemite on 1/3/15.
+//  Copyright (c) 2015 Yosemite. All rights reserved.
+//
+
+import Foundation
+
+// Struct will automatically generate initializer while class does not
+struct Photo {
+    var name:String
+    var fileName:String
+    var notes:String
+}
